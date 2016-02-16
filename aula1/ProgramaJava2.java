2db936f28425fe40ab8c8b7559cc7b5d84f632bd
carolmarostica <carolcefet99@gmail.com>
2016-02-16 16:32:31 -0200

    Erro de digitação
    printL corrigido para printLN

--- aula1/ProgramaJava2.java
+++ aula1/ProgramaJava2.java
@@ -1,6 +1,6 @@
 public class ProgramaJava2{
   public static void main(String[]args){
     String parametro = args[0];
-    System.out.printl("O valor digitado foi: " + parametro);
+    System.out.println("O valor digitado foi: " + parametro);
   }
 }
