b6e6f5fefe8c75b95528dd3e9422df9f1e344dba
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-02-26 09:48:44 -0300

    Exercicio 2_1

---
+++ exercicio2/Exercicio2_1.java
@@ -0,0 +1,9 @@
+
+public class Exercicio2_1{
+  public static void main(String[] args){
+    System.out.println("Tipo Abstrato de Dados: é uma especificação de um conjunto de dados e operações que podem ser executadas sobre esses dados. Ex: Classe."); 
+    System.out.println("Classe: é um tipo abstrato de dados, defini atributos e métodos, possui um conceito estático.");
+    System.out.println("Objeto: é a intância da classe, da vida a classe, possui um conceito dinâmico.");
+    System.out.println("Mensagens:é um texto pequeno que os objetos conseguem entender. Ex: Parâmetros.");
+  }
+}
