e96d013f9c39ca7020650f263d4ce095dd6967b7
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-02-26 08:25:55 -0300

    Update Exercicio1_2.java

--- exercicio1/Exercicio1_2.java
+++ exercicio1/Exercicio1_2.java
@@ -1,4 +1,4 @@
-public class Exercicio1_1{
+public class Exercicio1_2{
   public static void main(String[] args){
     String nome=args[0];
     System.out.println("O argumento digitado foi: " + nome);
