9fe02c9f917c209786293e141cfafa0ad00a8e1a
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-02-26 09:55:29 -0300

    Update Exercicio2_2.java

--- exercicio2/Exercicio2_2.java
+++ exercicio2/Exercicio2_2.java
@@ -1,6 +1,6 @@
 public class Exercicio2_2{
   public static void main(String[] args){
-    System.out.println("Qual é o propósito da palavra‐chave "new"? Explique o que acontece quando essa palavra‐chave é utilizada em um aplicativo:");
+    System.out.println("Qual é o propósito da palavra‐chave 'new'? Explique o que acontece quando essa palavra‐chave é utilizada em um aplicativo:");
     System.out.println("RESPOSTA: ");
   }
 }
