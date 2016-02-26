70cc6fa812f16f818e6207d77423afdc14192b18
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-02-26 08:42:03 -0300

    Exercicio1_6

--- exercicio1/Exercicio1_6.java
+++ exercicio1/Exercicio1_6.java
@@ -1,6 +1,6 @@
 
 public class Exercicio1_6{
   public static void main(String[] args){
-    System.out.println("");
+    System.out.println("O inteiro não possui ponto flutuante, diferente do float. Já o double é mais preciso que o float.");
   }
 }
