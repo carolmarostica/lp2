6a70b8466d0ec37c80015a698a0590495a28115b
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-04 07:44:27 -0300

    Teste para comparar strings

---
+++ Teste.java
@@ -0,0 +1,9 @@
+public class Teste{
+  public static void main(String[] args){
+      if (args[0].equals(args[1])){
+        System.out.println("Os parâmetros passados são iguais: " + args[1]);
+      }else {
+        System.out.println("Os parâmetros passados são diferentes. O primeiro parâmetro foi: " + args[0] + ", o segundo parâmetro foi: " + args[1]);
+      }
+  }
+}
