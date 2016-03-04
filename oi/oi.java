89c61366f95ee9de311e3a01f4a6413e2cdb7b10
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-04 07:54:11 -0300

    teste

---
+++ oi/oi.java
@@ -0,0 +1,25 @@
+public class oi{
+  public void Search(){
+    do{
+    
+    }while( args[0] );
+    
+    
+    
+    for(int x = 0; x < y; x++) {
+     
+     
+      if (args[0].equals(args[1])){
+        System.out.println("Os parâmetros passados são iguais: " + args[1]);
+      }else {
+        System.out.println("Os parâmetros passados são diferentes. O primeiro parâmetro foi: " + args[0] + ", o segundo parâmetro foi: " + args[1]);
+    }
+    
+    
+  }
+  
+  public static void main(String[] args){
+    
+    System.out.println("Conforme o parâmetro passado o polímedro encontrado foi: " + achei);
+  }
+}
