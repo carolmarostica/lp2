ac253e15636eb50eadb77355cf31e5823d82261f
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-04 07:46:19 -0300

    Update and rename Teste.java to ComparaString.java

--- ComparaString.java
+++ ComparaString.java
@@ -1,4 +1,4 @@
-public class Teste{
+public class ComparaString{
   public static void main(String[] args){
       if (args[0].equals(args[1])){
         System.out.println("Os parâmetros passados são iguais: " + args[1]);
