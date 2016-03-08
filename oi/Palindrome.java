21ff4741cab5cffede9767ab0c152c01227ff835
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-08 17:10:02 -0300

    Update and rename oi.java to Palindrome.java

---
+++ oi/Palindrome.java
@@ -0,0 +1,9 @@
+
+public class Palindrome {
+  public static void main(String[] args){
+    x=indexOf(args[0].lenght(0));
+    y=lastIndexOf(args[0].lenght(4));
+    w=args[0].substring(x,y);
+    System.out.println("Conforme o parâmetro passado o palíndrome encontrado foi: " + w);
+  }
+}
