94675714bd81d984959d4e7d969ea744faa3aca0
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-08 15:25:30 -0300

    Update tamanhoVariavel.java
    lenght = length*

--- oi/tamanhoVariavel.java
+++ oi/tamanhoVariavel.java
@@ -1,6 +1,6 @@
 public class tamanhoVariavel{
   public static void main(String[] args){
-    Int tamanho=args[0].lenght();
+    Int tamanho=args[0].length();
     System.out.println("O tamanho do parametro digitado foi: " + tamanho );
   }
 }
