6c24a94f4f5c790b92091358ffa16a312e323446
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-08 15:45:45 -0300

    Update oi.java

--- oi/oi.java
+++ oi/oi.java
@@ -1,25 +1,20 @@
-public class oi{
+class Palindrome{
   public void Search(){
+  
     do{
-    
     }while( args[0] );
-    
-    
-    
+    args[0].lenght;
     for(int x = 0; x < y; x++) {
-     
-     
-      if (args[0].equals(args[1])){
-        System.out.println("Os parâmetros passados são iguais: " + args[1]);
-      }else {
-        System.out.println("Os parâmetros passados são diferentes. O primeiro parâmetro foi: " + args[0] + ", o segundo parâmetro foi: " + args[1]);
     }
     
-    
+    if(){
+    }else{  
+    }
   }
+}
   
+public class Exercicio3 {
   public static void main(String[] args){
-    
-    System.out.println("Conforme o parâmetro passado o polímedro encontrado foi: " + achei);
+    System.out.println("Conforme o parâmetro passado o palíndrome encontrado foi: " + achei);
   }
 }
