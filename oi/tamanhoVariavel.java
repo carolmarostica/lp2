61df1aaea66653fe663dbd621a8954b8ff7fbd40
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-08 15:22:37 -0300

    Como descobrir o tamanho do parâmetro

---
+++ oi/tamanhoVariavel.java
@@ -0,0 +1,6 @@
+public class tamanhoVariavel{
+  public static void main(String[] args){
+    Int tamanho=args[0].lenght();
+    System.out.println("O tamanho do parametro digitado foi: " + tamanho );
+  }
+}
