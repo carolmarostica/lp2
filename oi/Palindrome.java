fb64e335ababfaf279ee19830835a55b37880323
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-08 17:14:26 -0300

    Update Palindrome.java

--- oi/Palindrome.java
+++ oi/Palindrome.java
@@ -1,8 +1,8 @@
 
 public class Palindrome {
   public static void main(String[] args){
-    int x=indexOf(args[0].lenght(0));
-    int y=lastIndexOf(args[0].lenght(4));
+    String x=indexOf(args[0].lenght(0));
+    String y=lastIndexOf(args[0].lenght(4));
     String w=args[0].substring(x,y);
     System.out.println("Conforme o parâmetro passado o palíndrome encontrado foi: " + w);
   }
