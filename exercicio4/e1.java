2393e64232a2513e311a923ffc4de2d22058ced2
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-08 16:20:15 -0300

    Update and rename 1.java to e1.java

--- exercicio4/e1.java
+++ exercicio4/e1.java
@@ -16,11 +16,11 @@ class Ponto2D {
   }
 }
 
-public class 1{
-  public static void main(String[] args){
-    Ponto2D p1= new Ponto2D(2.0, 2.0);
-    Ponto2D p2= new Ponto2D(3.0, 3.0);
-    p2=p1;
+public class e1 {
+  public static void main(String[] args) {
+    Ponto2D p1 = new Ponto2D(2.0, 2.0);
+    Ponto2D p2 = new Ponto2D(3.0, 3.0);
+    p2 = p1;
     p1.setX(4.0);
     System.out.println(p2.getX());
   }
