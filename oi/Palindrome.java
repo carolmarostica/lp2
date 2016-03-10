49ec74d3cebb79ef2bdaaef52430f7b5ec45b644
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-10 09:45:41 -0300

    Update Palindrome.java

--- oi/Palindrome.java
+++ oi/Palindrome.java
@@ -4,11 +4,11 @@ public class Palindrome {
     int q;
     p=0;
     q=p+4;
-    String w=args[0].substring(p,q);
+    String[] w=args[0].substring(p,q);
     for (int i=p; i<5; i++){
       for (int j=q; j>=0; j--){
         if((w[i]).equals(w[j])){
-          System.out.println("ok");
+          System.out.println(i+"-"+j +"/");
         }else{
         
         }
