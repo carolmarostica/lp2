be6bbe62dafd9239607eeafd7c7d3cdadbcd01b1
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-10 10:04:07 -0300

    Update Palindrome.java

--- oi/Palindrome.java
+++ oi/Palindrome.java
@@ -1,15 +1,10 @@
 public class Palindrome {
   public static void main(String[] args){
-    int p;
-    int q;
-    p=0;
-    q=p+4;
-    String[] w = new String[q+1];
-    for(int s=0; s<=q; s++){
-      w[s]=args[0].length(s);
-    }
-    for (int i=p; i<5; i++){
-      for (int j=q; j>=0; j--){
+    int p=0;
+    int q=p+4;
+    w=args[0].substring(p,q);
+    for (int i=0; i<5; i++){
+      for (int j=4; j>=0; j--){
         if((w[i]).equals(w[j])){
           System.out.println(i+"-"+j +"/");
         }else{
@@ -17,7 +12,6 @@ public class Palindrome {
         }
       }
     }
-    //w=args[0].substring(p,q);
     System.out.println("Conforme o parâmetro passado o palíndrome encontrado foi: " + w);
   }
 }
