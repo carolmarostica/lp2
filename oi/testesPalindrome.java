b4f012c15bbb353be4e6bc73480fb173c277e5d9
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-10 10:06:58 -0300

    Create testesPalindrome.java

---
+++ oi/testesPalindrome.java
@@ -0,0 +1,9 @@
+
+public class testePalindrome {
+  public static void main(String[] args){
+   String w=aegs[0];
+   String[] retornaArrayString(String w){
+     String[] string = new String[w.length];
+     return string;
+    }
+}
