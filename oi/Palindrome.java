6f0c01bab01b0990918b469f118b0fc560910336
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-15 14:53:49 -0300

    Added files via upload

--- oi/Palindrome.java
+++ oi/Palindrome.java
@@ -1,28 +1,22 @@
 public class Palindrome {
-  public static void main(String[] args){
-    int fim =0;
-    while( fim != 5){
-      int b = args[0].length();
-      System.out.println("Número de caracteres passados: " + b);
-      int p=0;
-      int q=p+4;
-      String w[];
-      w = new String[5];
-      w[0]=args[0].substring(p,p);
-      w[1]=args[0].substring(p+1,p+1);
-      w[2]=args[0].substring(p+2,p+2);
-      w[3]=args[0].substring(p+3,p+3);
-      w[4]=args[0].substring(q,q);
-      int j=4;
-      for (int i=0; i<b; i++){
-        if((w[i]).equals(w[j])){
-          System.out.println(i + "=" + j + w[0]);
-          j--;
-          fim++;
-        }else{
-          j++;
-        }
-      }  
-    }
-  }
-}
+
+   public static void main(String[] args){
+		String aux=StringBuffer(a).reverse().toString();		 
+		if(args[0]==aux){
+			System.out.println("palindrome" + args[0] + "/" + aux);
+				
+		}   
+	}      
+
+	public static void Teste(a){
+	    boolean retorno = false;
+		int tamanhoPalavra = a[0].length();
+		int tamanhoPalindrome = 5;
+		for (int i=0; i < tamanhoPalavra; i++){
+			/*if */	
+		}    
+		return retorno;
+  	}    
+ 
+}                                                                                                
+         
