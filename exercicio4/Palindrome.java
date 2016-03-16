e8c253925ac0dcdd6309e8fd48d1c2de5f256f87
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-16 17:51:10 -0300

    Create Palindrome.java

---
+++ exercicio4/Palindrome.java
@@ -0,0 +1,25 @@
+		/*Herbert, eu estava acabando o meu trabalho terça feira de tarde, logo depois de ter ido na sua sala.*/ 
+			/*	Alguem, provavelmente o tecnico de informatica, desligou todos os computadores e eu nao consegui*/
+			/*	 salvar a versão final. Estou encaminhando essa versão através do Windows, mas ela não está completa. */	
+		
+public class Palindrome {
+
+   public static void main(String[] args){
+		String aux=StringBuffer(a).reverse().toString();		 
+		if(args[0]==aux){
+			System.out.println("palindrome" + args[0] + "/" + aux);
+				
+		}   
+	}      
+
+	public static void Teste(a){
+	    boolean retorno = false;
+		int tamanhoPalavra = a[0].length();
+		int tamanhoPalindrome = 5;
+		for (int i=0; i < tamanhoPalavra; i++){
+	
+		}    
+		return retorno;
+  	}    
+ 
+}
